9779bfc22128f55efc44a22a990125b5da56983d
TiamatSKK <tiamatskk.dev@gmail.com>
2024-12-01 15:33:48 +0700

    implement upload file to get file date used multer

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const multer = require('multer');
+
 var app = express();
 
 app.use(cors());
@@ -11,10 +16,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+const uploadFile = multer({ dest: 'uploads' })
+
+app.post('/api/fileanalyse', uploadFile.single('upfile'), (req, res) => {
+  var info = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+
+  res.json(info);
 });
